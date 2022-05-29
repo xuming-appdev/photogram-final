83cacf6976ce3ee7da5f0033aff656abcd7029f3
xumingmonica101 <ming.xu@chicagobooth.edu>
2022-05-29 04:57:27 +0000

    Office hour question left

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -20,8 +20,8 @@ class FollowRequestsController < ApplicationController
   def create
     the_follow_request = FollowRequest.new
     the_follow_request.recipient_id = params.fetch("query_recipient_id")
-    the_follow_request.sender_id = params.fetch("query_sender_id")
-    the_follow_request.status = params.fetch("query_status")
+    the_follow_request.sender_id = session.fetch(:user_id)
+    #the_follow_request.status = params.fetch("query_status")
 
     if the_follow_request.valid?
       the_follow_request.save
